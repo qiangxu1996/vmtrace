5efb7b187e1ac9aec6937c8867352e0933e3b463
Qiang Xu <qiangxu1996@gmail.com>
2020-04-13 14:46:09 -0400

    empty gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+}
+
+group = "edu.purdue.dsnl"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
